de34fab9c7a966c8c145973e824bcce636fe964f
Smit-Zaveri <smitzaveri1003@gmail.com>
2024-12-24 15:18:54 +0530

    Refactor Firebase configuration to use environment variables and update .gitignore to exclude .env file

--- Library-Management-AdminSide/src/services/firebase.js
+++ Library-Management-AdminSide/src/services/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/storage";
 import { Timestamp} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_65TIYlVFlvHdWxUnklJ-SIxZ7qqTd_g",
-  authDomain: "library-management-system-smit.firebaseapp.com",
-  projectId: "library-management-system-smit",
-  storageBucket: "library-management-system-smit.firebasestorage.app",
-  messagingSenderId: "761944642976",
-  appId: "1:761944642976:web:03f9ffe6055ab36a9e80c5",
-  measurementId: "G-KZT8TZFMF3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
